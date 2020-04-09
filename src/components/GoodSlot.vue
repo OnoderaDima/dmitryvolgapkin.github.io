af24b2af10682e7298faea02e421e944b83fd265
OnoderaDima <Trigger-64@yandex.ru>
2020-04-09 14:25:55 +0300

    Add files via upload

--- src/components/GoodSlot.vue
+++ src/components/GoodSlot.vue
@@ -1,7 +1,7 @@
 <template>
     <div :class="{slot:true,slot__active:isActive()}">
         <h3>{{getMessage()}}</h3>
-        <Input type="number" max="10" @enter="chooseGood" :disabled="isDisabled()"/> 
+        <Input type="number" @enter="chooseGood" :disabled="isDisabled()"/> 
     </div>   
 </template>
 <script>
